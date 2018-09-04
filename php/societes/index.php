b61676c38f0bb17dd39083905671e3be6f10f4ad
JF <jfgeerinckx@mac.com>
2018-09-04 09:08:08 +0200

    ajout fichier php jf

---
+++ php/societes/index.php
@@ -0,0 +1,23 @@
+<?php
+include ("model.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/assets/css/style.css" rel="stylesheet" media="all" type="text/css"> 
+    <title>Document</title>
+</head>
+<body>
+
+  <ul>
+    <li>SOCIETES</li>
+    <li><?php include ("View.php"); ?></li>
+    </ul>
+
+</body>
+</html>
+
